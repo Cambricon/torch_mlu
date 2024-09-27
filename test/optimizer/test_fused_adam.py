ec89cf799fb03cb38856fded0f92f0a1403bbd75
shangang <shangang@cambricon.com>
2024-09-27 10:00:56 +0800

    [PYTORCH-12744]fix(bangc compile): fix cncc compile failed when multi-gcc version.

--- test/optimizer/test_fused_adam.py
+++ test/optimizer/test_fused_adam.py
@@ -10,6 +10,7 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(cur_dir + "/../")
 from common_utils import TEST_LARGETENSOR, largeTensorTest
 
+
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model, self).__init__()
@@ -42,6 +43,7 @@ class Model(torch.nn.Module):
         y = self.relu5(y)
         return y
 
+
 class ModelLinear(torch.nn.Module):
     def __init__(self):
         super(ModelLinear, self).__init__()
@@ -60,10 +62,11 @@ class ModelLinear(torch.nn.Module):
         y = self.linear2(y)
         y = self.relu2(y)
         y = self.linear3(y)
-        y = self.relu3(y)       
+        y = self.relu3(y)
         y = self.linear4(y)
         y = self.relu4(y)
         return y
+
 
 class TestFusedOptimizer(unittest.TestCase):
     def setUp(self, max_abs_diff=1e-3, max_rel_diff=1, iters=7):
@@ -112,13 +115,16 @@ class TestFusedOptimizer(unittest.TestCase):
             max_abs_diff_p = (p_ref - p_tst).abs().max().item()
             max_rel_diff_p = ((p_ref - p_tst) / p_ref).abs().max().item()
 
-            if max_abs_diff_p > max_abs_diff:  max_abs_diff = max_abs_diff_p
-            if max_rel_diff_p > max_rel_diff:  max_rel_diff = max_rel_diff_p
+            if max_abs_diff_p > max_abs_diff:
+                max_abs_diff = max_abs_diff_p
+            if max_rel_diff_p > max_rel_diff:
+                max_rel_diff = max_rel_diff_p
 
         return max_abs_diff, max_rel_diff
 
-    def gen_single_type_test(self, param_type=torch.float,
-                             device='mlu', *, skip_assert: bool = False):
+    def gen_single_type_test(
+        self, param_type=torch.float, device="mlu", *, skip_assert: bool = False
+    ):
         nelem = 278011
         # Some ref and test optimizers may require different set of options.
         # This is a quick workaround to add that functionality while making
@@ -126,9 +132,14 @@ class TestFusedOptimizer(unittest.TestCase):
         # If there is no "tst_options" field provided, safe to initialize
         # the test optimizer with the parameters of reference optimizer.
         for options in self.options_list:
-            tensor = torch.clamp(torch.rand(nelem).to(dtype=param_type, device=device), min=0.01, max=100.0)
-            ref_param, tst_param, ref_optim, tst_optim = \
-                self.gen_param_optim([tensor], options)
+            tensor = torch.clamp(
+                torch.rand(nelem).to(dtype=param_type, device=device),
+                min=0.01,
+                max=100.0,
+            )
+            ref_param, tst_param, ref_optim, tst_optim = self.gen_param_optim(
+                [tensor], options
+            )
 
             for i in range(self.iters):
                 self.gen_grad(ref_param, tst_param)
@@ -152,9 +163,9 @@ class TestFusedOptimizer(unittest.TestCase):
                 tst_options.pop("adam_w_mode")
                 tst_optim = torch.optim.AdamW(model.parameters(), **tst_options)
             fused_optim = self.fused_optim(model_.parameters(), **options)
-            memory_format=torch.contiguous_format
+            memory_format = torch.contiguous_format
             if len(input_size) == 4:
-                memory_format=torch.channels_last
+                memory_format = torch.channels_last
 
             for i in range(30):
                 x = torch.rand(input_size).to(memory_format=memory_format).mlu()
@@ -168,7 +179,7 @@ class TestFusedOptimizer(unittest.TestCase):
 
                 loss.backward()
                 tst_optim.step()
-                
+
                 # DUT
                 y = model_(x_)
                 loss_mlu = ((gt_ - y) ** 2).mean()
@@ -179,22 +190,51 @@ class TestFusedOptimizer(unittest.TestCase):
                 for module in zip(model.modules(), model_.modules()):
                     m = module[0]
                     m_ = module[1]
-                    if isinstance(m, torch.nn.Conv2d) or isinstance(m_, torch.nn.Linear):
-                        torch.testing.assert_close(m.weight.cpu(), m_.weight.cpu(), atol=1e-3, rtol=1e-3, equal_nan=True)
-                        torch.testing.assert_close(m.weight.grad.cpu(), m_.weight.grad.cpu(), atol=1e-3, rtol=1e-3, equal_nan=True)
+                    if isinstance(m, torch.nn.Conv2d) or isinstance(
+                        m_, torch.nn.Linear
+                    ):
+                        torch.testing.assert_close(
+                            m.weight.cpu(),
+                            m_.weight.cpu(),
+                            atol=1e-3,
+                            rtol=1e-3,
+                            equal_nan=True,
+                        )
+                        torch.testing.assert_close(
+                            m.weight.grad.cpu(),
+                            m_.weight.grad.cpu(),
+                            atol=1e-3,
+                            rtol=1e-3,
+                            equal_nan=True,
+                        )
 
                 # Init for next iteration
                 tst_optim.zero_grad()
                 fused_optim.zero_grad()
                 model_.load_state_dict(copy.deepcopy(model.state_dict()))
 
+
 class TestFusedAdam(TestFusedOptimizer):
     def setUp(self):
         super().setUp()
-        self.options_list = [{'lr':5e-4, 'betas':(0.9, 0.95), 'eps':1e-08,
-            'weight_decay': 0.1, 'amsgrad': False, 'adam_w_mode': True},
-            {'lr':5e-4, 'betas':(0.9, 0.95), 'eps':1e-08,
-            'weight_decay': 0.1, 'amsgrad': False, 'adam_w_mode': False}]
+        self.options_list = [
+            {
+                "lr": 5e-4,
+                "betas": (0.9, 0.95),
+                "eps": 1e-08,
+                "weight_decay": 0.1,
+                "amsgrad": False,
+                "adam_w_mode": True,
+            },
+            {
+                "lr": 5e-4,
+                "betas": (0.9, 0.95),
+                "eps": 1e-08,
+                "weight_decay": 0.1,
+                "amsgrad": False,
+                "adam_w_mode": False,
+            },
+        ]
         self.fused_optim = torch_mlu.optimizers.FusedAdam
 
     def test_float(self):
@@ -209,7 +249,7 @@ class TestFusedAdam(TestFusedOptimizer):
     def test_bfloat16(self):
         self.gen_single_type_test(param_type=torch.bfloat16, skip_assert=True)
 
-    @unittest.skipIf(torch.mlu.device_count()<2, "more than 1 GPU required")
+    @unittest.skipIf(torch.mlu.device_count() < 2, "more than 1 GPU required")
     def test_multi_device(self):
         devices = ("mlu:0", "mlu:1")
         for current_dev, tensor_dev in product(devices, devices):
@@ -218,14 +258,29 @@ class TestFusedAdam(TestFusedOptimizer):
 
     def test_adam_option(self):
         nelem = 1
-        options_list = [{'lr':0.01, 'betas':(0.6, 0.9), 'eps':3e-06,
-            'weight_decay':0.001, 'amsgrad':False, 'adam_w_mode': True},
-            {'lr':0.01, 'betas':(0.6, 0.9), 'eps':3e-06,
-            'weight_decay':0.001, 'amsgrad':False, 'adam_w_mode': False}]
+        options_list = [
+            {
+                "lr": 0.01,
+                "betas": (0.6, 0.9),
+                "eps": 3e-06,
+                "weight_decay": 0.001,
+                "amsgrad": False,
+                "adam_w_mode": True,
+            },
+            {
+                "lr": 0.01,
+                "betas": (0.6, 0.9),
+                "eps": 3e-06,
+                "weight_decay": 0.001,
+                "amsgrad": False,
+                "adam_w_mode": False,
+            },
+        ]
         for adam_option in options_list:
-            tensor = torch.rand(nelem, dtype=torch.float, device='mlu')
-            ref_param, tst_param, ref_optim, tst_optim = \
-                self.gen_param_optim([tensor], adam_option)
+            tensor = torch.rand(nelem, dtype=torch.float, device="mlu")
+            ref_param, tst_param, ref_optim, tst_optim = self.gen_param_optim(
+                [tensor], adam_option
+            )
 
             for i in range(self.iters):
                 self.gen_grad(ref_param, tst_param)
@@ -259,16 +314,31 @@ class TestFusedAdam(TestFusedOptimizer):
 
     def test_frozen_model(self):
         nelem = 1
-        options_list = [{'lr':0.01, 'betas':(0.6, 0.9), 'eps':3e-06,
-            'weight_decay':0, 'amsgrad':False, 'adam_w_mode': True},
-            {'lr':0.01, 'betas':(0.6, 0.9), 'eps':3e-06,
-            'weight_decay':0, 'amsgrad':False, 'adam_w_mode': False}]
+        options_list = [
+            {
+                "lr": 0.01,
+                "betas": (0.6, 0.9),
+                "eps": 3e-06,
+                "weight_decay": 0,
+                "amsgrad": False,
+                "adam_w_mode": True,
+            },
+            {
+                "lr": 0.01,
+                "betas": (0.6, 0.9),
+                "eps": 3e-06,
+                "weight_decay": 0,
+                "amsgrad": False,
+                "adam_w_mode": False,
+            },
+        ]
         for adam_option in options_list:
-            tensor = torch.rand(nelem, dtype=torch.float, device='mlu')
-            ref_param, tst_param, ref_optim, tst_optim = \
-                self.gen_param_optim([tensor], adam_option)
+            tensor = torch.rand(nelem, dtype=torch.float, device="mlu")
+            ref_param, tst_param, ref_optim, tst_optim = self.gen_param_optim(
+                [tensor], adam_option
+            )
 
-            #Add an empty param group which may occur for pipeline parallel p-tuning
+            # Add an empty param group which may occur for pipeline parallel p-tuning
             tst_optim.add_param_group({"params": []})
 
             for i in range(self.iters):
@@ -283,14 +353,67 @@ class TestFusedAdam(TestFusedOptimizer):
     @unittest.skipIf(
         TEST_LARGETENSOR, "run largeTensorCases by `TEST_LARGETENSOR=TRUE` or `--large`"
     )
-    @largeTensorTest("44GB")
+    @largeTensorTest("78GB")
     def test_fused_adam_large(self):
-        tensor = torch.clamp(torch.rand(2147483660).to(dtype=torch.float, device="mlu"), min=0.01, max=100.0)
-        ref_param = [torch.nn.Parameter(tensor.clone())]
-        fused_optim = self.fused_optim(ref_param, **self.options_list[0])
-        for i in range(self.iters):
-            ref_param[0].grad = torch.rand_like(ref_param[0])
-            fused_optim.step()
+        # add custom cases
+        size_list = [
+            (
+                23986176,
+                10214400,
+                12278784,
+                11993088,
+                23986176,
+                44445696,
+                11983872,
+                47972352,
+                11003088,
+                23986176,
+                10214400,
+                11983872,
+                23986176,
+                10214400,
+                12278784,
+                11993088,
+                23986176,
+                44445696,
+                11983872,
+                47972352,
+                11003088,
+                23986176,
+                10214400,
+                11983872,
+                11003088,
+                23986176,
+                10214400,
+            ),
+            (11003088, 23986176, 10214400, 11983872),
+            (16100, 12288),
+            (12288),
+            (2147483660),
+        ]
+        num_list = [27, 4, 26, 89, 1]
+        repeat_list = [False, False, True, True]
+        for size, num, repeat in zip(size_list, num_list, repeat_list):
+            tensors = []
+            for i in range(num):
+                if repeat is True:
+                    tensors.append(
+                        torch.nn.Parameter(
+                            torch.rand(size, dtype=torch.float, device="mlu")
+                        )
+                    )
+                else:
+                    tensors.append(
+                        torch.nn.Parameter(
+                            torch.rand(size[i], dtype=torch.float, device="mlu")
+                        )
+                    )
+            fused_optim = self.fused_optim(tensors, **self.options_list[0])
+            for i in range(self.iters):
+                for tensor in tensors:
+                    tensor.grad = torch.rand_like(tensor)
+                fused_optim.step()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
