c09f4d4b911b162507ae3a9c432d8f5b2f8ea962
zhouyusong <zhouyusong@cambricon.com>
2024-12-19 19:55:39 +0800

    [PYTORCH-13228]feat(op): implement fast_path for foreach_copy

--- torch_mlu/csrc/aten/operators/cnnl/foreach_ops.cpp
+++ torch_mlu/csrc/aten/operators/cnnl/foreach_ops.cpp
@@ -27,32 +27,113 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+#include <ATen/native/ForeachUtils.h>
 
 #include "aten/utils/foreach_check_utils.h"
 #include "aten/operators/cnnl/cnnl_kernel.h"
 #include "aten/operators/cnnl/internal/cnnl_internal.h"
+#include "aten/operators/cnnl/internal/foreach_common_utils.h"
 
 namespace torch_mlu {
 namespace ops {
 
+typedef std::pair<std::vector<at::Tensor>, std::vector<at::Tensor>> copy_pair;
+
+inline bool can_use_fast_route(
+    at::ArrayRef<at::TensorList> tensorLists,
+    at::ArrayRef<at::Scalar> scalarList = {},
+    bool does_op_promote_integer_inputs_to_float = false) {
+  return at::native::_check_tensors_share_device_and_dtype(tensorLists) &&
+      at::native::_check_tensors_do_type_promotion_with_scalars(
+             tensorLists[0],
+             scalarList,
+             does_op_promote_integer_inputs_to_float);
+}
+
+std::map<size_t, copy_pair> process_input_params(
+    const at::TensorList& self,
+    const at::TensorList& src) {
+  auto is_diff_stride = [](const IntArrayRef& size,
+                           const IntArrayRef& left_stride,
+                           const IntArrayRef& right_stride) -> bool {
+    const size_t size_size = size.size();
+    for (const auto dim : c10::irange(size_size)) {
+      if (size[dim] == 1)
+        continue;
+      if (left_stride[dim] != right_stride[dim]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  std::map<size_t, copy_pair> copy_map;
+  for (const auto i : c10::irange(self.size())) {
+    if (self[i].dtype() != src[i].dtype()) {
+      CNLOG(INFO) << "src and dst has different dtype"
+                  << " dst dtype " << self[i].dtype() << " and src dtype "
+                  << src[i].dtype();
+      copy_map[0].first.push_back(self[i]);
+      copy_map[0].second.push_back(src[i]);
+    } else if (self[i].sizes() != src[i].sizes()) {
+      CNLOG(INFO) << "src and dst has different sizes"
+                  << " dst sizes " << self[i].sizes() << " and src sizes "
+                  << src[i].sizes();
+      copy_map[0].first.push_back(self[i]);
+      copy_map[0].second.push_back(src[i]);
+    } else if (is_diff_stride(
+                   self[i].sizes(), self[i].strides(), src[i].strides())) {
+      CNLOG(INFO) << "src and dst has different strides"
+                  << " dst strides " << self[i].strides() << " and src strides "
+                  << src[i].strides();
+      copy_map[0].first.push_back(self[i]);
+      copy_map[0].second.push_back(src[i]);
+    } else {
+      size_t data_type_bits = self[i].element_size();
+      copy_map[data_type_bits].first.push_back(self[i]);
+      copy_map[data_type_bits].second.push_back(src[i]);
+    }
+  }
+  return copy_map;
+}
 void cnnl__foreach_copy_(
     at::TensorList self,
     at::TensorList src,
     const bool non_blocking) {
-  // slow path
   at::native::check_foreach_api_restrictions(self, src);
   if (!torch_mlu::can_use_fast_route(
           self, src, /* does_op_promote_integer_inputs_to_float */ false)) {
+    at::native::foreach_tensor_copy_list_kernel_slow_(self, src, non_blocking);
     for (const auto i : c10::irange(self.size())) {
       self[i].copy_(src[i], non_blocking);
     }
   } else {
-    TORCH_WARN_ONCE("MLU foreach copy only support slow path now.");
-    for (const auto i : c10::irange(self.size())) {
-      self[i].copy_(src[i], non_blocking);
+    auto copy_map = process_input_params(self, src);
+    for (const auto& pair : copy_map) {
+      if (pair.first == 0) {
+        at::native::foreach_tensor_copy_list_kernel_slow_(
+            pair.second.first, pair.second.second, non_blocking);
+      } else {
+        auto handle = getCurrentHandle();
+        ForeachOPTensorScalarHandle<1, 1, false> tensor_desc_ptr(
+            {pair.second.second, pair.second.first}, {});
+        const int64_t tensor_num = tensor_desc_ptr.get_tensor_num();
+        if (tensor_num == 0)
+          return;
+        auto [input_desc_array, input_ptr_array] =
+            tensor_desc_ptr.template get_input_tensor_desc_and_ptr<0>();
+        auto [output_desc_array, output_ptr_array] =
+            tensor_desc_ptr.template get_output_tensor_desc_and_ptr<0>();
+        TORCH_CNNL_CHECK(cnnlForeachCopy(
+            handle,
+            tensor_num,
+            input_desc_array,
+            input_ptr_array,
+            output_desc_array,
+            output_ptr_array));
+      }
     }
   }
 }
-
 } // namespace ops
 } // namespace torch_mlu
