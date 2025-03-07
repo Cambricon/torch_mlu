1066be9422961bb03bc5fcd3b5191fd75efca435
xushuo <xushuo@cambricon.com>
2025-03-07 10:32:18 +0800

    Revert "[PYTORCH-13868] feat(header): remove unused headers"

--- torch_mlu/csrc/framework/generator/generator_impl.h
+++ torch_mlu/csrc/framework/generator/generator_impl.h
@@ -31,10 +31,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
 #include <unordered_set>
-#include <ATen/core/TensorBase.h>
 #include <c10/core/GeneratorImpl.h>
 #include <ATen/core/Generator.h>
-#include "utils/Export.h"
+#include "framework/core/device.h"
+#include "aten/cnnl/cnnlHandle.h"
 
 namespace torch_mlu {
 
